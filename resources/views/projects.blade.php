26803a83c18858da67b24d5abe407eda89090b12
Yohanrumbiak <yrumbiakwanma@gmail.com>
2026-05-25 11:31:46 +0900

    Mengedit informasi pada halan project

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -12,19 +12,11 @@
         <div class="col-md-4 mb-4">
             <div class="card p-4 h-100 shadow project-card text-light">
                 <h5>Sistem Absensi QR Code</h5>
-                <p>Aplikasi absensi guru berbasis Laravel dengan fitur laporan dan grafik keterlambatan.</p>
+                <p>Aplikasi absensi guru mengunakan QR Code berbasis Laravel v10 dengan fitur laporan dan grafik keterlambatan guru yang sering terlambat.</p>
                 <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
             </div>
         </div>
-
-        <div class="col-md-4 mb-4">
-            <div class="card p-4 h-100 shadow project-card text-light">
-                <h5>Sistem SAW Seleksi Mahasiswa</h5>
-                <p>Sistem pendukung keputusan menggunakan metode SAW berbasis Laravel.</p>
-                <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
-            </div>
-        </div>
-
+        
         <div class="col-md-4 mb-4">
             <div class="card p-4 h-100 shadow project-card text-light">
                 <h5>Website Tour & Travel</h5>
@@ -35,24 +27,32 @@
         
         <div class="col-md-4 mb-4">
             <div class="card p-4 h-100 shadow project-card text-light">
-                <h5>Sistem Absensi QR Code</h5>
-                <p>Aplikasi absensi guru berbasis Laravel dengan fitur laporan dan grafik keterlambatan.</p>
-                <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
-            </div>
-        </div>
-
-        <div class="col-md-4 mb-4">
-            <div class="card p-4 h-100 shadow project-card text-light">
                 <h5>Sistem SAW Seleksi Mahasiswa</h5>
-                <p>Sistem pendukung keputusan menggunakan metode SAW berbasis Laravel.</p>
+                <p>Sistem pendukung keputusan menggunakan metode SAW berbasis Codigniter4.</p>
                 <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
             </div>
         </div>
 
         <div class="col-md-4 mb-4">
             <div class="card p-4 h-100 shadow project-card text-light">
-                <h5>Website Tour & Travel</h5>
-                <p>Website pemesanan paket wisata dan event dengan fitur rating dan booking.</p>
+                <h5>Website Portfolio</h5>
+                <p>Saya juga telah membagun/mengembangkan lebih dari 1 website portfolio yang menampilkan profile saya dan project-project nyata yang pernah saya buat.</p>
+                <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card p-4 h-100 shadow project-card text-light">
+                <h5>Project PHP-CRUD</h5>
+                <p>Project yang menampilkan fungsi Create, Read, Update, dan Delete menggunakan bahasa pemrograman PHP.</p>
+                <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card p-4 h-100 shadow project-card text-light">
+                <h5>Fitur Image Animation</h5>
+                <p>Saya juga berhasil membuat fitur animasi gambar pada website portfolio.</p>
                 <a href="#" class="btn btn-outline-light btn-sm">View Project</a>
             </div>
         </div>
